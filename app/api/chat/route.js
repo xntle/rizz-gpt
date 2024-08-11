41777cde8e7d0e0406fd6696245d807ca41e1249
An Le <115895584+xntle@users.noreply.github.com>
2024-08-12 02:51:13 +0700

    update api key

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -11,8 +11,8 @@ const systemPrompt = `You are an AI-powered customer support assistant for Amazo
 4. If you are unable to answer the question, you can ask the user to contact customer service.
 `;
 
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 // POST function to handle incoming requests
 export async function POST(req) {
@@ -20,6 +20,7 @@ export async function POST(req) {
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   });
+
   
   const data = await req.json() // Parse the JSON body of the incoming request
 
